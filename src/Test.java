a3e5c6a70ba21e2afbd11e8ed154fbdda7239479
CHAN Kwong Chung Joshua <kcjchanab@connect.ust.hk>
2019-04-10 14:49:12 +0800

    Kim-version

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@
 public class Test {
 	public static void main(String arg[]) {
 		System.out.println("Hello World - Trump version");
+		//this is comment
+		System.out.println("Hello World - Kim version");
 	}
 }
